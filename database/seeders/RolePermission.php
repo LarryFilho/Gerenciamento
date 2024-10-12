d11d7c8c109b7f278aaff93c38dcd2e376a446da
LarryFilho <111398359+LarryFilho@users.noreply.github.com>
2024-10-12 18:36:26 -0300

    Update RolePermission.php

--- database/seeders/RolePermission.php
+++ database/seeders/RolePermission.php
@@ -4,11 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-<<<<<<< Updated upstream
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
-=======
->>>>>>> Stashed changes
+use App\Models\User;
 
 class RolePermission extends Seeder
 {
@@ -19,29 +17,11 @@ class RolePermission extends Seeder
      */
     public function run()
     {
-<<<<<<< Updated upstream
-        // Fetch all permissions
         $permissions = Permission::all();
 
-        // Create or find the 'porteiro' role
         $role = Role::firstOrCreate(['name' => 'porteiro']);
 
-        // Sync permissions to the role
         $role->syncPermissions($permissions);
-=======
-        $user = User::create([
-            'name' => 'Douglas', 
-            'email' => 'douglas@gmail.com',
-            'password' => bcrypt('senha')
-        ]);
-    
-        $role = Role::create(['name' => 'porteiro']);
-     
-        $permissions = Permission::pluck('id','id')->all();
-   
-        $role->syncPermissions($permissions);
-     
-        $user->assignRole([$role->id]);
->>>>>>> Stashed changes
+        
     }
 }
